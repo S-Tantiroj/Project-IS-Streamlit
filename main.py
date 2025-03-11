e925efca2292193d3df3b3aa6a19347aa07f0a7e
S-Tantiroj <s.tantiroj00@gmail.com>
2025-03-11 22:07:49 +0700

    test streamlit

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+
+with st.echo():
+    st.title("Project Intelligent System")
+    st.write("Test")
+
+    st.markdown("## my markdown")
+    code = '''def hello():
+        print("hello")'''
+
+    run_btn = st.button("Predict Traffic")
+    if run_btn:
+        st.code(code, language='python')
+
+    cols = st.columns(2)
+    with cols[0]:
+        age_inp = st.number_input("Input Number: ")
+        st.markdown(f"Your number is {age_inp}")
+
+
+
+    st.markdown("# NLP Task")
+    with cols[1]:
+        text_inp = st.text_input("Input your text")
+        word_tokenize = "|".join(text_inp.split())
+        st.markdown(f"{word_tokenize}")
+
+    df = pd.DataFrame({
+        'first column': [1,2,3,4],
+        'second column': [10,20,30,40]
+    })
+    st.dataframe(df)
+    show_plot_btn = st.button("Show Chart!")
+    if show_plot_btn:
+        st.line_chart(data=df, x= 'first column', y= 'second column')
+
