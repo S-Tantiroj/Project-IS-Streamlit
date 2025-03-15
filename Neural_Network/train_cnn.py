4f94350106d9979290fc48bb9178497b3a680992
Siwakorn <s.tantiroj00@gmail.com>
2025-03-15 22:54:52 +0700

    change folder neural network

---
+++ Neural_Network/train_cnn.py
@@ -0,0 +1,9 @@
+# import libraries
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+# กำหนดพาธข้อมูล
+train_dir = '../Neural_Network/train'  # แก้เป็นพาธของคุณ
+valid_dir = '../Neural_Network/valid'
+test_dir = '../Neural_Network/test'
